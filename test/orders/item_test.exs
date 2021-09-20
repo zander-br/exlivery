82ac0f4e9ace066981b1d96920d95ea4aead1101
Anderson Santos <anderson.feitoza.santos@gmail.com>
2021-09-20 07:48:16 -0300

    when there is an invalid price, returns an error

--- test/orders/item_test.exs
+++ test/orders/item_test.exs
@@ -21,5 +21,13 @@ defmodule Exlivery.Orders.ItemTest do
 
       assert response == expected_response
     end
+
+    test "when there is an invalid price, returns an error" do
+      response = Item.build("Pizza Mussarela", :pizza, "invalid_price", 1)
+
+      expected_response = {:error, "Invalid price"}
+
+      assert response == expected_response
+    end
   end
 end
